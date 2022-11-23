0b15f53f8f32443e601ad8d7eb15d4cc896d8742
Mircea Dogaru <mircea.dogaru.md@gmail.com>
2022-11-23 23:43:25 +0000

    Add snowballs

--- src/Player/Player.gd
+++ src/Player/Player.gd
@@ -1,16 +1,37 @@
 extends KinematicBody2D
 
+var snowball = preload("res://Snowball/Snowball.tscn")
+
 export (String) var action_input = "interact_p1"
+export (Vector2) var aim_direction = Vector2.RIGHT
 export (int) var jump_speed = -1000
 export (int) var gravity = 1500
 
+export (float) var min_jump_height = 500
+export (float) var max_jump_height = 1000
+export (float) var jump_duration = .5
+
+export (float) var min_throw_distance = 500
+export (float) var max_throw_distance = 1000
+export (float) var throw_duration = .5
+
 var velocity = Vector2.ZERO
+var current_jump_height = 0.0
+var current_throw_distance = 0.0
 
 func _physics_process(delta):
 	velocity.y += gravity * delta
 	
 	var on_floor = is_on_floor()
 	if Input.is_action_just_pressed(action_input):
+		current_jump_height = min_jump_height
+		current_throw_distance = min_throw_distance
+	
+	if Input.is_action_pressed(action_input):
+		current_jump_height = clamp(current_jump_height + (max_jump_height - min_jump_height) * delta / jump_duration, min_jump_height, max_jump_height)
+		current_throw_distance = clamp(current_throw_distance + (max_throw_distance - min_throw_distance) * delta / throw_duration, min_throw_distance, max_throw_distance)
+	
+	if Input.is_action_just_released(action_input):
 		if on_floor:
 			jump()
 		else:
@@ -19,7 +40,11 @@ func _physics_process(delta):
 	velocity = move_and_slide(velocity, Vector2.UP)
 			
 func jump():
-	velocity.y = jump_speed
+	print(current_jump_height)
+	velocity.y = -current_jump_height
 
 func throw_snowball():
-	pass
+	var new_snowball = snowball.instance()
+	get_tree().root.add_child(new_snowball)
+	new_snowball.transform.origin = transform.origin + aim_direction
+	new_snowball.apply_central_impulse(aim_direction * current_throw_distance)
