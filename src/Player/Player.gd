0a19e6ce9fceb43d4bcabfc4e9e91c1a5e8a664c
Mircea Dogaru <mircea.dogaru.md@gmail.com>
2022-11-25 11:41:19 +0000

    Implement AI

--- src/Player/Player.gd
+++ src/Player/Player.gd
@@ -9,20 +9,42 @@ export (Vector2) var aim_direction = Vector2.RIGHT
 export (int) var jump_speed = -1000
 export (int) var gravity = 1500
 
-export (float) var min_jump_height = 500
-export (float) var max_jump_height = 1000
+export (float) var min_jump_height = 500.0
+export (float) var max_jump_height = 1000.0
 export (float) var jump_duration = .5
 
-export (float) var min_throw_distance = 500
-export (float) var max_throw_distance = 1000
+export (float) var min_throw_distance = 500.0
+export (float) var max_throw_distance = 1000.0
 export (float) var throw_duration = .5
+
+export (bool) var is_ai = false
+export (float) var ai_input_wait_min = 0.05
+export (float) var ai_input_wait_max = 0.3
 
 var velocity = Vector2.ZERO
 var current_jump_height = 0.0
 var current_throw_distance = 0.0
 
+var ai_input_wait = 0.0
+var ai_current_input_wait = 0.0
+
 func _ready():
 	set_scale(Vector2(aim_direction.x, 1))
+	
+func _process(delta):
+	if not is_ai:
+		return
+	do_ai_stuff(delta)	
+	
+func do_ai_stuff(delta):
+	ai_current_input_wait += delta
+	if ai_current_input_wait > ai_input_wait:
+		ai_input_wait = rand_range(ai_input_wait_min, ai_input_wait_max)
+		ai_current_input_wait = 0.0
+		if Input.is_action_pressed(action_input):
+			Input.action_release(action_input)
+		else:
+			Input.action_press(action_input)
 
 func _physics_process(delta):
 	velocity.y += gravity * delta
@@ -55,6 +77,7 @@ func throw_snowball():
 	new_snowball.transform.origin = transform.origin + aim_direction
 	new_snowball.apply_torque_impulse(rand_range(-10, 10))
 	new_snowball.apply_central_impulse(aim_direction * current_throw_distance)
+	$AnimationPlayer.play("Toss")
 
 func _on_hat_hit(body):
 	if body.player_who_threw == self:
--- src/Snowball/Snowball.gd
+++ src/Snowball/Snowball.gd
@@ -3,12 +3,11 @@ extends RigidBody2D
 var player_who_threw : Node
 var has_collided = false
 
-func _process(delta):
+func _process(_delta):
 	if has_collided and $Blast.emitting == false:
 		queue_free()
 
 func _on_Snowball_body_entered(body):
-	print("it hurts: " + body.name)
 	if body == player_who_threw:
 		return
 	
