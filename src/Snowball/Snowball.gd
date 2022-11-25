a101caf6601656636ac94676838ce699269a3f68
Mircea Dogaru <mircea.dogaru.md@gmail.com>
2022-11-25 11:12:57 +0000

    Add snowball particles

--- src/Snowball/Snowball.gd
+++ src/Snowball/Snowball.gd
@@ -1,10 +1,21 @@
 extends RigidBody2D
 
 var player_who_threw : Node
+var has_collided = false
+
+func _process(delta):
+	if has_collided and $Blast.emitting == false:
+		queue_free()
 
 func _on_Snowball_body_entered(body):
 	print("it hurts: " + body.name)
 	if body == player_who_threw:
 		return
-		
-	queue_free()
+	
+	call_deferred("stop_snowball")
+
+func stop_snowball():
+	mode = RigidBody2D.MODE_STATIC
+	$CollisionShape2D.disabled = true
+	$Blast.restart()
+	has_collided = true
